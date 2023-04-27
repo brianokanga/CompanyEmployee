c96aa305ac96081e2a3adf9d1c9c11a17d9e7537
Brianokanga <briannokanga@gmail.com>
2023-04-27 13:41:15 +0300

    Added Employee model

--- Entities/Models/Company.cs
+++ Entities/Models/Company.cs
@@ -6,6 +6,10 @@ namespace Entities.Models
 {
 	public class Company
 	{
-        public Guid Id { get; set; }
-    }
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string Country { get; set; }
+		public ICollection<Employee> Employees { get; set; }
+	}
 }
